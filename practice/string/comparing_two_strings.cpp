7db2a01715573e48074d6246e90898930f171d42
ChiragSidana <chiragsidana2709@gmail.com>
2021-04-25 14:00:00 +0100

    compare_string

---
+++ practice/string/comparing_two_strings.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1="xyz";
+    string s2="zzz";
+if(s1.compare(s2)==0){
+    cout<<"strings are equal"<<endl;
+}
+else{
+    cout<<s1.compare(s2)<<endl;
+}
+    return 0;
+}
