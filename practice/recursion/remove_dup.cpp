eda17c153c7c327069dadd7c4f847441fcc513a4
ChiragSidana <chiragsidana2709@gmail.com>
2021-05-04 14:00:00 +0100

    remove_dup

---
+++ practice/recursion/remove_dup.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+string removedup(string s){
+    if(s.length()==0){
+        return "";
+    }
+    char ch=s[0];
+    string ans=removedup(s.substr(1));
+    if(ch==ans[0]){
+        return ans;
+    }
+    return (ch+ans);
+}
+int main(){
+    cout<<removedup("aaaabbbbccccdddeee")<<endl;
+    return 0;
+}
