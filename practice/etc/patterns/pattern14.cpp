6bc63d9435214b368d60ca8d4e22a7e3f5fd36dd
ChiragSidana <chiragsidana2709@gmail.com>
2021-05-28 14:00:00 +0100

    pattern_14

---
+++ practice/etc/patterns/pattern14.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+     #ifndef ONLINE_JUDGE
+           freopen("input.txt", "r", stdin);
+           freopen("output.txt", "w", stdout);
+       #endif
+     
+
+    int i,j,n;
+    cin>>n;
+    for(i=1;i<=3;i++){
+        for(j=1;j<=n;j++){
+            if(((i+j)%4==0) || (i==2 && j%4==0)){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+                
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
