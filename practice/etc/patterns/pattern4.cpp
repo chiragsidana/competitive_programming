606668ec8928714da1cfd51facbe862751a8d044
ChiragSidana <chiragsidana2709@gmail.com>
2021-05-31 14:00:00 +0100

    pattern_4

---
+++ practice/etc/patterns/pattern4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+       #ifndef ONLINE_JUDGE
+           freopen("input.txt", "r", stdin);
+           freopen("output.txt", "w", stdout);
+       #endif
+     int n,m,i,j;
+     cin>>n;
+     m=n;
+     for(i=1;i<=n;i++){
+         for(j=1;j<=n;j++){
+            if(j>=m){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+         }
+         m=m-1;
+         cout<<endl;
+     }
+     
+
+    return 0;
+}
