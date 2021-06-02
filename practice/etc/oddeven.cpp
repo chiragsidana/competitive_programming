d55c97fe5fe2fb4a920c5593ba4eade0476979c3
ChiragSidana <chiragsidana2709@gmail.com>
2021-06-02 14:00:00 +0100

    odd_even

---
+++ practice/etc/oddeven.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+       #ifndef ONLINE_JUDGE
+           freopen("input.txt", "r", stdin);
+           freopen("output.txt", "w", stdout);
+       #endif
+
+       int n;
+       cin>>n;
+       if(n%2==0){
+           cout<<"even";
+       }   
+       else{
+           cout<<"odd";
+       }
+
+
+
+    return 0;
+}
