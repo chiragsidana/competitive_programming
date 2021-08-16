23bd7c28f10aca3caaa87bde08fe0cd40fc2ee3d
ChiragSidana <chiragsidana2709@gmail.com>
2021-08-16 14:00:00 +0100

    find_me

---
+++ codechef/Find_Me.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int linearsearch(int arr[],int n,int k){
+for(int i=0;i<n;i++){
+    if(arr[i]==k){
+        return 1;
+    }
+  
+    
+}
+return -1;
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+  
+   cout<<linearsearch(arr,n,k);
+    return 0;
+}
