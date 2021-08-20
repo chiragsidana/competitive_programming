d0d11433874f965ffcc7f7621fe93fe2fb3ef2d3
ChiragSidana <chiragsidana2709@gmail.com>
2021-08-20 14:00:00 +0100

    passengers_in_bus

---
+++ codechef/Bus_full_of_passengers.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,q;
+        cin>>n>>m>>q;
+        char ch;
+        bool b=true;
+        int ele;
+        vector<pair<char,int> > v;
+        for(int i=0;i<q;i++){
+            cin>>ch;
+            cin>>ele;
+            v.push_back(make_pair(ch,ele));
+        }
+       int arr[n+1]={0};
+       int count=0;
+       for(int i=0;i<q;i++){
+           if((v[i].first)=='+'){
+               arr[v[i].second]++;
+               count++;
+           }
+           else if((v[i].first)=='-'){
+               arr[v[i].second]--;
+               count--;
+           }
+            if((count>m)||((arr[v[i].second])<0)){
+                cout<<"Inconsistent"<<endl;
+                b=false;
+                break;
+            }
+            
+       }
+    if(b)
+    cout<<"Consistent"<<endl;
+}
+return 0;
+}
