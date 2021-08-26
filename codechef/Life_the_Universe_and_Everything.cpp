dd6434c1e0045097b1f18939fcc75ea3d6b7da9c
ChiragSidana <chiragsidana2709@gmail.com>
2021-08-26 18:10:22 +0530

    codechef_dsa_series

---
+++ codechef/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int t=100;
+    int n;
+    while(t--){
+        cin>>n;
+        if(n==42){
+            break;
+        }
+        else{
+            cout<<n<<endl;
+        }
+    }
+}
+int main(){
+    solve();
+    return 0;
+}
