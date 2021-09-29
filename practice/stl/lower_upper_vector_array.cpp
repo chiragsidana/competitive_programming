73a0566ba7f40ce80cd10ca81f906ab88ae4c2fc
ChiragSidana <chiragsidana2709@gmail.com>
2021-09-29 16:44:07 +0530

    lower_upper_bound

---
+++ practice/stl/lower_upper_vector_array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // array
+    //vector
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }cout<<endl;
+    // int *ptr=lower_bound(a,a+n,26);
+     auto it=upper_bound(a.begin(),a.end(),25);
+    if(it==a.end()){
+        cout<<"not found";
+        return 0;
+    }
+    cout<<(*it)<<endl;
+
+}
