857ddec11709117bb82924ccf29de35995edf646
ChiragSidana <chiragsidana2709@gmail.com>
2021-12-14 23:16:48 +0530

    flat_binary_tree

---
+++ practice/binary_tree/flat_binary_tree.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+int data;
+struct Node* left;
+struct Node* right;
+
+Node(int val){
+    data=val;
+    left=NULL;
+    right=NULL;
+}
+};
+void inorder(struct Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void flatten(Node* root){
+    if(root==NULL or (root->left==NULL or root->right==NULL )){
+        return;
+    }
+    if(root->left!=NULL){
+        flatten(root->left);
+        Node*temp=root->right;
+        root->right=root->left;
+        root->left=NULL;
+        Node*t=root->right;
+        while(t->right!=NULL){
+            t=t->right;
+        }
+        t->right=temp;
+    }
+    flatten(root->right);
+}
+int main(){
+struct Node* root=new Node(4);
+root->left=new Node(9);
+root->right=new Node(5);
+
+root->left->left=new Node(1);
+root->left->right=new Node(3); 
+
+
+root->right->right=new Node(6);
+
+flatten(root);
+inorder(root);
+return 0;
+}
