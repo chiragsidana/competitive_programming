f0ff5abd539be44d4c86a77b2db6d1c611671202
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-10 22:27:48 +0530

    CFR-764

---
+++ codeforces/C_Division_by_Two_and_Permutation.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    vi v(n);
+    rep(i,0,n){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    map<int,bool> mp;
+    rep(i,0,n){
+        if(v[i]<=n and mp[v[i]]==false){
+            mp[v[i]]=true;
+        }
+        else{
+            while(v[i]/2 >0){
+                v[i]=v[i]/2;
+                if(v[i]<=n and mp[v[i]]==false){
+                    mp[v[i]]=true;
+                    break;
+                }
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        if(mp[i]==false){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
