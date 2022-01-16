7ceeac9043f211e23e6b93c05bfe2f761c89bb11
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-16 15:41:44 +0530

    longest_subsequence

---
+++ practice/dp/longest_increasing_subsequence.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+using namespace std;
+const int N=1e5+2;
+int dp[N];
+
+// int lis(vi &a,int n){
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }
+//     dp[n]=1;
+//     rep(i,0,n){
+//         if(a[n]>a[i]){
+//             dp[n]=max(dp[n],1+lis(a,i));
+//         }
+//     }
+//     return dp[n];
+// }
+int main()
+{
+    // rep(i,0,N){
+    //     dp[i]=-1;
+    // }
+    // int n;cin>>n;
+    // vi a(n);
+    // rep(i,0,n){
+    //     cin>>a[i];
+    // }
+    // cout<<lis(a,n-1)<<endl;
+
+    int n;cin>>n;
+    vi a(n);
+    rep(i,0,n){
+        cin>>a[i];
+    }
+    vi dp(n,1);
+    int ans=0;
+    rep(i,1,n){
+        rep(j,0,i){
+            if(a[i]>a[j])
+            dp[i]=max(dp[i],1+dp[j]);
+        }
+        ans=max(ans,dp[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
