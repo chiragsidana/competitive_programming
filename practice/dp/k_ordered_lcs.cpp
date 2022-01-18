ab884d0ef651fe38314665a1ed8cdc2933eb4f61
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-18 19:16:16 +0530

    k_order_lcs

---
+++ practice/dp/k_ordered_lcs.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+using namespace std;
+const int N=1e3+2;
+int n,m,k;
+vi a,b;
+int dp[2005][2005][7];
+int solve(int i,int j,int k){
+    if(i==n or j==m){
+        return 0;
+    }
+    if(dp[i][j][k]!=-1)
+        return dp[i][j][k];
+    int c1(0),c2(0),c3(0);
+    if(a[i]==b[j])
+        c1=1+solve(i+1,j+1,k);
+    if(k>0)
+        c2=1+solve(i+1,j+1,k-1);
+    c3=max(solve(i+1,j,k),solve(i,j+1,k));
+    return dp[i][j][k]=max({c1,c2,c3});
+}
+int main()
+{
+    memset(dp,-1,sizeof(dp));
+cin>>n>>m>>k;
+a=vi(n);
+b=vi(m);
+for(auto &i:a)
+    cin>>i;
+for(auto &i:b){
+    cin>>i;
+}
+cout<<solve(0,0,k);
+    return 0;
+}
