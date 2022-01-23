e0944f6e649fb7765e679ff25fb25891a774b226
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-23 22:35:41 +0530

    jan_cookoff_22

---
+++ codechef/Palindromes_Not_Allowed.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+// #define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void solve()
+{
+    int n;
+    cin>>n;
+    int x;
+    if(n%26){
+         x=(n/26)+1;
+
+    }
+    else{
+        x=n/26;
+    }
+    string s="";
+    
+    for(int i=0;i<x;i++){
+        // while(n--){
+            for(int j=97;j<123;j++){
+               
+                if(s.size()==n)break;
+                s+=j;
+            }
+        // }
+    }
+    cout<<s<<endl;
+
+}
+int main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
