5896474c48ae263ed2e09cc85dc8ed68ffcacd73
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-25 14:35:53 +0530

    sub string

---
+++ practice/string_algorithm/substr.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+int main()
+{
+    string s="na";
+    string t="apna collegena";
+    int S=s.size(),T=t.size();
+    rep(i,0,T-S+1){
+        if(t.substr(i,S)==s){
+            cout<<"found at index"<<i<<endl;
+        }
+    }
+
+    return 0;
+}
