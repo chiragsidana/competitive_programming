22a30b1cfabfb475590b01523b35eeba96d9cf03
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-30 22:06:47 +0530

    CFR-769

---
+++ codeforces/B_Roof_Construction.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void solve()
+{
+    int n;cin>>n;
+    int temp=1;
+    while(temp*2<n) temp<<=1;
+    for(int i=n-1;i>=temp;i--) cout<<i<<" ";
+    cout<<0<<" ";
+    for(int i=1;i<temp;i++) cout<<i<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
