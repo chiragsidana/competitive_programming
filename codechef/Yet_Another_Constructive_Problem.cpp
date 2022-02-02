0e17800609503f8b658fa738e40350a827aa0440
ChiragSidana <chiragsidana2709@gmail.com>
2022-02-02 22:59:59 +0530

    Starter-24

---
+++ codechef/Yet_Another_Constructive_Problem.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void solve()
+{
+    int x;
+    cin>>x;
+    int t=log2(x);
+    int C=(int)pow(2,t+1)+x;
+    int B=x;
+    int A=0;
+    cout<<A<<" "<<B<<" "<<C<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
