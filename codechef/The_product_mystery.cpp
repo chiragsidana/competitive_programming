bd55be5a65dbcbf1567a1005b2a139a022a96e43
ChiragSidana <chiragsidana2709@gmail.com>
2022-02-23 22:06:36 +0530

    STARTER-27

---
+++ codechef/The_product_mystery.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+int gcd(int a, int b)
+{
+    // Everything divides 0
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+    // base case
+    if (a == b)
+        return a;
+  
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+void solve()
+{
+    int b,c;cin>>b>>c;
+    int chirag=gcd(b,c);
+    int sidana=(b*c)/chirag;
+    cout<<sidana/b<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
