f1e7a2b58234e40bbdd442e6a21ee49d40f8fd39
ChiragSidana <chiragsidana2709@gmail.com>
2022-03-10 22:06:38 +0530

    ECFR-124

---
+++ codeforces/A_Playoff.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void solve()
+{
+    int n;cin>>n;
+    int chirag=pow(2,n);
+    cout<<chirag-1<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
