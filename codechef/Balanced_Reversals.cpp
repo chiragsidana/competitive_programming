3ec28805e215a51294e298b4f45a594dca022755
ChiragSidana <chiragsidana2709@gmail.com>
2022-03-17 10:04:56 +0530

    Starter-30

---
+++ codechef/Balanced_Reversals.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;cin>>s;
+    bool ok=true;
+    rep(i,0,n-1){
+        if(s[i]>s[i+1]){
+            ok=false;
+        }
+    }
+    if(ok){
+        cout<<s<<endl;
+        return;
+    }
+    int count0=0;
+    int count1=0;
+    string ans="";
+    rep(i,0,n){
+        if(s[i]=='0'){
+            count0++;
+        }
+        else {
+            count1++;}
+
+    }
+    
+    rep(i,0,count0){
+        ans+='0';
+    }
+    rep(i,0,count1){
+        ans+='1';
+    }
+    cout<<ans<<endl;
+}
+signed main()
+{
+    FastIO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
