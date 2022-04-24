06ad7bfe526b05795bb88e8e775277a2993df3e2
ChiragSidana <chiragsidana2709@gmail.com>
2022-04-24 15:19:12 +0530

    print1-N-backtracking

---
+++ practice/recursion/print_1-n_backtracking.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void f(int i,int n){
+    if(i<1){
+        return;
+    }
+    f(i-1,n);
+    cout<<i<<" ";
+}
+int main()
+{
+    int n;cin>>n;
+    f(n,n);
+    return 0;
+}
