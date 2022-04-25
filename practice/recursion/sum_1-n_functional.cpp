fa8b7f0819146cd9c5d8a93816ed7bbffde1a67f
ChiragSidana <chiragsidana2709@gmail.com>
2022-04-25 14:51:52 +0530

    sum_1-n_functional

---
+++ practice/recursion/sum_1-n_functional.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+int f(int n){
+    if(n==0) return 0;
+    return (n+f(n-1));
+}
+int main()
+{
+    int n;cin>>n;
+   cout<<f(n);
+    return 0;
+}
