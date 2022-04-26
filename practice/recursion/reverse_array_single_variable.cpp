a418fdb76726bdc2810d75b8ddf74fc4e217a89c
ChiragSidana <chiragsidana2709@gmail.com>
2022-04-26 12:29:35 +0530

    reverse array

---
+++ practice/recursion/reverse_array_single_variable.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+//#define int long long
+#define endl "\n"
+#define PI 3.141592653589793238
+#define MOD 1000000007
+#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using namespace std;
+const int N=1e3+2;
+void f(int i,vi &a){
+   int n=a.size();
+    if(i>=n/2) return;
+    swap(a[i],a[n-i-1]);
+    f(i+1,a);
+
+}
+int main()
+{
+    int n;cin>>n;
+    vi a(n);
+    rep(i,0,n){
+        cin>>a[i];
+    }
+    f(0,a);
+    rep(i,0,n){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
